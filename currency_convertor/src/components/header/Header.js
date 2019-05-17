7d05b709d63199cef53d504dad6cefad64f98ac5
LiubomyrYurchyshyn <42535795+LiubomyrYurchyshyn@users.noreply.github.com>
2019-05-17 19:30:01 +0300

    Create Header.js

---
+++ currency_convertor/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+  render(){
+    return(
+      <section className = "header">
+        <h1>Currency converter</h1>
+      </section>
+    )
+  }
+}
